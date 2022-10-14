d8fad196f5f65ba8070db252030072a01de7ce30
NICOLAS CRISTIEL ENDRIZZI <nicolas.endrizzi@acad.univates>
2022-10-13 21:41:06 -0300

    adicionado pesquisa em largura e profundidade

--- src/main/java/com/data/structure/graphsearch/GraphSearch.java
+++ src/main/java/com/data/structure/graphsearch/GraphSearch.java
@@ -5,10 +5,15 @@
 package com.data.structure.graphsearch;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.graphstream.algorithm.Dijkstra;
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
+import org.graphstream.graph.Path;
 import org.graphstream.graph.implementations.SingleGraph;
 
 /**
@@ -17,18 +22,17 @@ import org.graphstream.graph.implementations.SingleGraph;
  */
 public class GraphSearch {
 
-    public static final String[] NODES
-            = {
-                "Lajeado",
-                "Arroio do Meio",
-                "Estrela",
-                "Bento Gonçalves",
-                "Encantado",
-                "Venâncio Aires",
-                "Taquari",
-                "Arvorezinha",
-                "Teutônia",
-                "Carlos Barbosa"};
+    public static final String[] NODES = {
+            "Lajeado",
+            "Arroio do Meio",
+            "Estrela",
+            "Bento Gonçalves",
+            "Encantado",
+            "Venâncio Aires",
+            "Taquari",
+            "Arvorezinha",
+            "Teutônia",
+            "Carlos Barbosa" };
 
     public static final Integer NODE_LAJEADO = 0;
     public static final Integer NODE_ARROIO_DO_MEIO = 1;
@@ -72,8 +76,12 @@ public class GraphSearch {
 
     }
 
+    public static List<Node> visited = new ArrayList<>();
+
     public static void main(String[] args) {
         Graph graph = new SingleGraph("GraphSearch");
+        graph.setAutoCreate(true);
+        graph.setStrict(false);
         graph.display();
 
         List<Node> nodes = new ArrayList<>();
@@ -103,16 +111,14 @@ public class GraphSearch {
         graph.addEdge(NodeId.ID_EB.ordinal() + "", nodes.get(NODE_ENCANTADO), nodes.get(NODE_BENTO));
         graph.addEdge(NodeId.ID_EA.ordinal() + "", nodes.get(NODE_ENCANTADO), nodes.get(NODE_ARVOREZINHA));
 
-        graph.addAttribute("ui.quality");
-        graph.addAttribute("ui.antialias");
         graph.setAttribute("ui.stylesheet", ""
                 + "edge {"
                 + "   size: 2px;"
                 + "   fill-color: gray;"
                 + "}");
 
-        for (Edge edge : graph.getEdgeSet().toArray(new Edge[0])) {
-            edge.setAttribute("ui.label", NodeId.values()[Integer.parseInt(edge.getId())].getWeight());
+        for (Edge edge : graph.getEdgeSet().stream().collect(Collectors.toList())) {
+            edge.setAttribute("weight", NodeId.values()[Integer.parseInt(edge.getId())].getWeight());
         }
 
         int i = 0;
@@ -121,7 +127,93 @@ public class GraphSearch {
             node.setAttribute("ui.label", NODES[i]);
             i++;
         }
-        
-        
+
+        Node lajeado = graph.getNode(NODES[NODE_LAJEADO]);
+
+        System.out.println("DEEP FIRST ITERATOR");
+        deepFirst(lajeado);
+
+        System.out.println("\nDEEP BREADTH ITERATOR");
+        breadthFirst(lajeado);
+
+        // System.out.println("\nSMALL PATH");
+        // shorstPath(graph);
+
+    }
+
+    public static void deepFirst(Node first) {
+        Iterator<Node> deepFirst = first.getDepthFirstIterator();
+
+        while (deepFirst.hasNext()) {
+            System.out.println("Node -> " + deepFirst.next());
+        }
+    }
+
+    public static void breadthFirst(Node first) {
+        Iterator<Node> breadthFirst = first.getBreadthFirstIterator();
+
+        while (breadthFirst.hasNext()) {
+            System.out.println("Node -> " + breadthFirst.next());
+        }
+    }
+
+    public static void shorstPath(Graph graph) {
+        graph.display(false);
+
+        // Edge lengths are stored in an attribute called "length"
+        // The length of a path is the sum of the lengths of its edges
+        Dijkstra dijkstra = new Dijkstra(Dijkstra.Element.EDGE, null, "length");
+
+        // Compute the shortest paths in g from A to all nodes
+        dijkstra.init(graph);
+        dijkstra.setSource(graph.getNode(NODES[NODE_LAJEADO]));
+        dijkstra.compute();
+
+        // Print the lengths of all the shortest paths
+        for (Node node : graph)
+            System.out.printf("%s->%s:%10.2f%n", dijkstra.getSource(), node,
+                    dijkstra.getPathLength(node));
+
+        // Color in blue all the nodes on the shortest path form A to B
+        for (Node node : dijkstra.getPathNodes(graph.getNode(NODES[NODE_BARBOSA])))
+            node.setAttribute("ui.style", "fill-color: blue;");
+
+        // Color in red all the edges in the shortest path tree
+        for (Edge edge : dijkstra.getTreeEdges())
+            edge.setAttribute("ui.style", "fill-color: red;");
+
+        // Print the shortest path from A to B
+        System.out.println(dijkstra.getPath(graph.getNode(NODES[NODE_BARBOSA])));
+
+        // Build a list containing the nodes in the shortest path from A to B
+        // Note that nodes are added at the beginning of the list
+        // because the iterator traverses them in reverse order, from B to A
+        List<Node> list1 = new ArrayList<Node>();
+        for (Node node : dijkstra.getPathNodes(graph.getNode(NODES[NODE_BARBOSA])))
+            list1.add(0, node);
+
+        // A shorter but less efficient way to do the same thing
+        List<Node> list2 = dijkstra.getPath(graph.getNode(NODES[NODE_BARBOSA])).getNodePath();
+
+        // cleanup to save memory if solutions are no longer needed
+        dijkstra.clear();
+
+        // Now compute the shortest path from A to all the other nodes
+        // but taking the number of nodes in the path as its length
+        dijkstra = new Dijkstra(Dijkstra.Element.NODE, null, null);
+        dijkstra.init(graph);
+        dijkstra.setSource(graph.getNode("A"));
+        dijkstra.compute();
+
+        // Print the lengths of the new shortest paths
+        for (Node node : graph)
+            System.out.printf("%s->%s:%10.2f%n", dijkstra.getSource(), node,
+                    dijkstra.getPathLength(node));
+
+        // Print all the shortest paths between A and F
+        Iterator<Path> pathIterator = dijkstra.getAllPathsIterator(graph
+                .getNode("F"));
+        while (pathIterator.hasNext())
+            System.out.println(pathIterator.next());
     }
 }
